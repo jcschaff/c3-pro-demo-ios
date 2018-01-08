a3b14359d0b2f14d8925d38f8f1e5254e46e8839
Jim Schaff <schaff@uchc.edu>
2018-01-08 12:08:54 -0500

    EncryptedDataQueue, auth/api urls, fake public.crt & key_id
    Use EncryptedDataQueue with delegate (for key_id)
       calls queue.ready() to start listening in C3DemoServer
    Hard-coded Auth/API urls (/etc/hosts spoofs api.jcschaff.net
       to local machine), Auth on port 8888, API on port 8889
    public certificate needs to be in main application bundle
       in public.crt with hard coded key_id = "public_key_id"
    Registration is lazy, must accept consent and they submit
       questionnaire (sometimes to it twice ???).
    Need to test JWT timeout.

--- c3-pro-demo-ios/C3DemoServer.swift
+++ c3-pro-demo-ios/C3DemoServer.swift
@@ -26,32 +26,52 @@ final class C3DemoServer {
         return instance
     }()
     
-    var queue: DataQueue
+    class C3DemoQueueDelegate: EncryptedDataQueueDelegate {
+        var key_id: String
+        init(key_id: String){
+            self.key_id = key_id
+        }
+        func encryptedDataQueue(_ queue: EncryptedDataQueue, wantsEncryptionForResource resource: Resource, requestMethod: FHIRRequestMethod) -> Bool {
+            return true
+        }
+        
+        func keyIdentifierForEncryptedDataQueue(_ queue: EncryptedDataQueue) -> String? {
+            return key_id
+        }
+    }
+
+    var queue: EncryptedDataQueue
+    var delegate: C3DemoQueueDelegate
 
     init() {
         //super.init()
-        //let hostname = "jamess-mbp-2.j4home.net"
-        //let hostname = "jamess-mbp-2.local"
-        //let hostname = "Jamess-MacBook-Pro-2.local"
-        
-        let baseurl = "https://api.jcschaff.net:8888"
-        //let baseurl = "https://2amoveu7z2.execute-api.us-east-1.amazonaws.com/alpha"
-        //let baseurl = "https://api.jcschaff.net/c3pro/alpha"
+        let baseurl_auth = "https://api.jcschaff.net:8888/c3pro"
+        //let baseurl_auth = "https://2amoveu7z2.execute-api.us-east-1.amazonaws.com/alpha/c3pro"
+        //let baseurl_auth = "https://api.jcschaff.net/c3pro/alpha/c3pro"
+        let baseurl_api = "https://api.jcschaff.net:8889/c3pro"
+        //let baseurl_api = "https://2amoveu7z2.execute-api.us-east-1.amazonaws.com/alpha/c3pro"
+        //let baseurl_api = "https://api.jcschaff.net/c3pro/alpha/c3pro"
         let my_auth = [
             "client_name": "My Awesome App",
             //"client_id": "MY Client ID",
-            "authorize_uri": "\(baseurl)/auth",
-            "registration_uri": "\(baseurl)/register",
+            "authorize_uri": "\(baseurl_auth)/auth",
+            "registration_uri": "\(baseurl_auth)/register",
             "authorize_type": "client_credentials",
             ] as OAuth2JSON
-        queue = DataQueue(baseURL: URL(string: "\(baseurl)/")!, auth: my_auth)
+        // encBaseURL: URL, publicCertificateFile: String
         
-//        //
-//        // DANGER ... DEBUG URL Session Delegate allows man-in-the-middle attacks (using self-signed certs or bogus Certificate Authorities).
-//        // using for now, because convenient for testing only.
-//        //
-//        queue.sessionDelegate = OAuth2DebugURLSessionDelegate(host: hostname)
+        // this cert file should be "bundled" with the app????
+        //let certFile = "/Users/schaff/.c3pro/iOS/public"
+        let certFile = "public"
         
+        queue = EncryptedDataQueue(baseURL: URL(string: "\(baseurl_api)/")!,
+                                   auth: my_auth,
+                                   encBaseURL: URL(string: "\(baseurl_api)/fhirenc/")!,
+                                   publicCertificateFile: certFile)
+        
+        
+        delegate = C3DemoQueueDelegate(key_id: "public_key_id")
+        queue.delegate = delegate
         queue.logger = OAuth2DebugLogger(OAuth2LogLevel.trace)
         queue.onBeforeDynamicClientRegistration = { url in
             let registration = OAuth2DynRegAppStore()
@@ -62,5 +82,20 @@ final class C3DemoServer {
                 ] as OAuth2StringDict
             return registration
         }
+        queue.ready(callback: { (error: FHIRError?) in
+            if let e = error {
+                print(e)
+            }
+            return
+        })
+//        queue.registerIfNeeded(callback:{ (oauth: OAuth2JSON?, error: Error?) in
+//            if let o = oauth {
+//                print(o)
+//            }
+//            if let e = error {
+//                print(e)
+//            }
+//            return
+//        })
     }
 }
